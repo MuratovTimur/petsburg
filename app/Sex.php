dab38f01a0666b343da710249f5ce0257b1fb6d1
MuratovTimur <u0054.muratov@gmail.com>
2018-02-26 00:20:04 -0500

    add table sex

---
+++ app/Sex.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//половая принадлежность животного
+class Sex extends Model
+{
+    //
+}
---
+++ database/migrations/2018_02_26_051444_create_sexes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSexesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sexes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sex_en', 25);
+            $table->string('sex_ru', 25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sexes');
+    }
+}
