4bc49324e7fdf8cdadfff524539ae3ea9c528803
MuratovTimur <u0054.muratov@gmail.com>
2018-02-26 01:18:51 -0500

    add table Breeds

---
+++ app/Breed.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//справочник всех пород для каждого типа житвотного
+//код породы по Международному справонику(пример собаки FCI)
+class Breed extends Model
+{
+    //
+}
---
+++ database/migrations/2018_02_26_060900_create_breeds_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBreedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('breeds', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type_pet_id');
+            $table->string('breed_en',128);
+            $table->string('breed_ru',128);
+            $table->string('code',30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('breeds');
+    }
+}
