627fd76ee7c3b852c80f8d4b45dd1db16719b5db
MuratovTimur <u0054.muratov@gmail.com>
2018-02-26 00:24:10 -0500

    add table languages

---
+++ app/Language.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//перечень языков используемых в системе
+//сокращенное название до 3 символов
+class Language extends Model
+{
+    //
+}
