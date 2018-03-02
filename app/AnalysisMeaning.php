124b3ffdae015951af99bfd77ffc0e5f0c71fd86
MuratovTimur <u0054.muratov@gmail.com>
2018-03-01 21:24:29 -0500

    add table AnalysisMeaning

---
+++ app/AnalysisMeaning.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//единицы измерения для результатов табл(resultAnalysis)анализов
+class AnalysisMeaning extends Model
+{
+    //
+}
