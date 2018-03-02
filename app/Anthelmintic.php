a3feb2f02604afc512db66f6207a0126e71d5938
MuratovTimur <u0054.muratov@gmail.com>
2018-03-01 21:04:35 -0500

    add table Anthelminitics add table Ectoparasites

---
+++ app/Anthelmintic.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//перечень противоглистное для всех типов животных
+class Anthelmintic extends Model
+{
+    //
+}
---
+++ app/Ectoparasite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//перечень средств против эктопаразитов для всех типов животных
+class Ectoparasite extends Model
+{
+    //
+}
