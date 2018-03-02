55d9780c9f0beb2047110d2e198e787b61ba9478
MuratovTimur <u0054.muratov@gmail.com>
2018-03-01 21:16:03 -0500

    add table ResultAnalysis

---
+++ app/ResultAnalysis.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//результаты анализов(название измеряемых параметров)
+//для всех типов животных на разных языках
+class ResultAnalysis extends Model
+{
+    //
+}
---
+++ database/migrations/2018_03_02_021124_create_result_analyses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResultAnalysesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('result_analyses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type_pet_id');
+            $table->string('name_en', 128);
+            $table->string('name_ru', 128);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('result_analyses');
+    }
+}
