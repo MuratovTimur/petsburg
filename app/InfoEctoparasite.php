83181a8c7bfa8f27dae3cddaac83b8d112050981
MuratovTimur <u0054.muratov@gmail.com>
2018-03-11 13:19:42 -0400

    add table InfoEctoparasite add table ReminderEctoparasite

---
+++ app/InfoEctoparasite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//информация о сделанных Ectoparasite(от блох и клищей)
+class InfoEctoparasite extends Model
+{
+    //
+}
---
+++ app/ReminderEctoparasite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//напоминание что нужно сделать Ectoparasite(от блох и клищей)
+class ReminderEctoparasite extends Model
+{
+    //
+}
